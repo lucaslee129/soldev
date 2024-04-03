91b3a6a2db71e87fbf4ae2ef65836c59c1edb2cc
awesomeLucas <adfr486233@gmail.com>
2024-04-03 17:21:32 +0800

    feat: get the keypairs from dotenv file and run using esrun

--- server.ts
+++ server.ts
@@ -1,12 +1,15 @@
-import { Keypair } from '@solana/web3.js';
-// import 'dotenv/config';
-// import { getKeypairFromEnvironment } from '@solana-developers/helpers';
+// import { Keypair } from '@solana/web3.js';
+import dotenv from 'dotenv';
+dotenv.config();
+import { getKeypairFromEnvironment } from '@solana-developers/helpers';
 
-// @ Generate New Key Pair
-const keypair = Keypair.generate();
+// // @ Generate New Key Pair
+// const keypair = Keypair.generate();
 
 // @ Import existing Key Pair
-// const keypair = getKeypairFromEnvironment('SECRET_KEY');
+const keypair = getKeypairFromEnvironment('SECRET_KEY');
 console.log('The public key is:', keypair.publicKey.toBase58());
 console.log('The secret key is:', keypair.secretKey);
-console.log('✅ Finished!');
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
