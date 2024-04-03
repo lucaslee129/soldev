4c5659b567109c1a8810161d1363943dfbb1bf84
awesomeLucas <adfr486233@gmail.com>
2024-04-03 17:15:23 +0800

    feat: generate the keypair and run using the esrun

---
+++ server.ts
@@ -0,0 +1,12 @@
+import { Keypair } from '@solana/web3.js';
+// import 'dotenv/config';
+// import { getKeypairFromEnvironment } from '@solana-developers/helpers';
+
+// @ Generate New Key Pair
+const keypair = Keypair.generate();
+
+// @ Import existing Key Pair
+// const keypair = getKeypairFromEnvironment('SECRET_KEY');
+console.log('The public key is:', keypair.publicKey.toBase58());
+console.log('The secret key is:', keypair.secretKey);
+console.log('✅ Finished!');
