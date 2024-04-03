48487c2a3307278a118c689a9eb2a8e1860130bd
awesomeLucas <adfr486233@gmail.com>
2024-04-03 17:10:27 +0800

    feat: get secret key from dotenv file

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import { Keypair } from '@solana/web3.js';
+import 'dotenv/config';
+import { getKeypairFromEnvironment } from '@solana-developers/helpers';
 
-const keypair = Keypair.generate();
+// // @ Generate New Key Pair
+// const keypair = Keypair.generate();
 
+// @ Import existing Key Pair
+const keypair = getKeypairFromEnvironment('SECRET_KEY');
 console.log('The public key is:', keypair.publicKey.toBase58());
 console.log('The secret key is:', keypair.secretKey);
