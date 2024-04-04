53970f0d2515d9d547afec4e93468d2135eb4d45
awesomeLucas <adfr486233@gmail.com>
2024-04-04 11:50:43 +0800

    feat: read data from the devnet

--- server.ts
+++ server.ts
@@ -1,18 +1,21 @@
-const {
-  Connection,
-  PublicKey,
-  clusterApiUrl,
-  LAMPORTS_PER_SOL,
-} = require('@solana/web3.js');
+const { Connection, PublicKey, LAMPORTS_PER_SOL } = require('@solana/web3.js');
 
 const main = async () => {
-  const connection = new Connection(clusterApiUrl('devnet'));
-  const address = new PublicKey('CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN');
+  // const suppliedPublicKeys = process.argv[2];
+  // if (!suppliedPublicKeys) {
+  //   throw new Error('Provide a public key to check the balance of!');
+  // }
+
+  const connection = new Connection(
+    'https://api.devnet.solana.com',
+    'confirmed'
+  );
+  const address = new PublicKey('3ci8RBupy8bQsRKZqLZJXkKsGL6xAoi7t7Q55GEihdCo');
   const balance = await connection.getBalance(address);
   const balanceInSol = balance / LAMPORTS_PER_SOL;
 
   console.log(
-    `The balance of the account at ${address} is ${balanceInSol} lamports`
+    `The balance of the account at ${address} is ${balanceInSol} SOL`
   );
   console.log(`✅ Finished!`);
 };
